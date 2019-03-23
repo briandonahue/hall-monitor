e3a75bd9dbc588714b810969594cc8d152eb69e2
Brian Donahue <brian.donahue@vitreosolutions.com>
2019-03-23 14:26:12 -0400

    Move out one function, needs more cleanup

---
+++ apps-script/parse-progress-data.ts
@@ -0,0 +1,45 @@
+import { GradeData } from './GradeData'
+
+const parseProgressData = (msg: GoogleAppsScript.Gmail.GmailMessage) =>  {
+  let body = msg.getPlainBody()
+
+  const msgDate = msg.getDate()
+  const data = new GradeData()
+  data.updatedDate = `${msgDate.getMonth() + 1}/${msgDate.getDate()}/${msgDate.getFullYear()}`
+  data.markingPeriod = /Grading period\s*:\s*(.*)/.exec(body)[1]
+  data.course = /Course\s*:\s*(.*)/.exec(body)[1]
+  data.teacher = /Instructor\s*:\s*(.*)/.exec(body)[1]
+  data.overallGrade = /Current overall grade\**\s*:\s*(.*)/.exec(body)[1].trim()
+  
+  let classInfoRE = /.*\sGrade:\s.*\r/g
+//  let classInfoRE = /.*\r/g
+  let classInfo =  body.match(classInfoRE)
+  
+  data.assignments = classInfo.map(function (g) {
+    const sp = g.split("Grade:")
+    const dateCourse = /(\d\d\/\d\d\/\d\d\d\d)\s+(.*)/.exec(sp[0].trim())
+ 
+    const gradeInfo = sp[1].split("(")
+    const letterGrade = gradeInfo[0].trim()
+   
+    const gradeDetails = gradeInfo[1].split(")")
+    const gradeNote = gradeDetails.length > 1 ? gradeDetails[1].trim() : ""
+    const scores = gradeDetails[0].split(" = ")
+    const score = scores[0]
+    const percentage = scores.length > 1 ? scores[1].trim() : ""
+ 
+    
+    const grade = {
+      date: dateCourse[1].trim(),
+      name: dateCourse[2].trim(),
+      grade: letterGrade,
+      score: score,
+      percentage: percentage,
+      notes: gradeNote === "^" ? 'Exempt' : ''
+    }
+    return grade
+  })
+  return data;
+}
+
+export = parseProgressData
