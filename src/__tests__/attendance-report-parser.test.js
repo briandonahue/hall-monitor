430695d98856f197a1f23a455557652f053f077f
Brian Donahue <brian.donahue@vitreosolutions.com>
2019-03-23 14:26:24 -0400

    Basic parsing logic

--- src/__tests__/attendance-report-parser.test.js
+++ src/__tests__/attendance-report-parser.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const fs = require('fs')
 
 const emailText = fs.readFileSync('./src/__tests__/data/attendance-report-1.txt').toString()
@@ -8,13 +9,20 @@ describe('attendance report parser', () => {
     const splits = newText.split('Expression ')
     for (const part of splits) {
       if (part.startsWith('HR')) {
-        let match = (/\d\d\/\d\d\/\d\d\d\d\s+\w+/gm).exec(part)
-        console.log(match)
+        const lines = part.split('\n')
+        for (const line of lines) {
+          const match = (/(\d\d\/\d\d\/\d\d\d\d)\s+(\w+)/).exec(line)
+          if (match) {
+            console.log(`${match[1]}: ${match[2]}`)
+          }
+        }
+        /*
         do {
           console.log(match)
 
           match = (/\d\d\/\d\d\/\d\d\d\d\s+\w+/gm).exec(part)
         } while (match != null)
+    */
       }
     }
 
